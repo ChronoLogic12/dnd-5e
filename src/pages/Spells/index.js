ad33e4b4c00b3206e39f25d5f6f4a41d862680fe
ChronoLogic <msuttie12@hotmail.com>
2022-08-20 21:53:45 +0100

    Feat: Create search functionality for spells list and add loader

--- src/pages/Spells/index.js
+++ src/pages/Spells/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import materialize from 'materialize-css';
 
 import { SpellCard, Pagination } from '../../components';
 
+import style from '../../styles/styles.css';
+
 export default () => {
 	const [spells, setSpells] = useState([]);
+	const [filteredSpells, setFilteredSpells] = useState([]);
 	const [error, setError] = useState(null);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [spellsPerPage] = useState(30);
+	const [search, setSearch] = useState('');
+	const [noResults, setNoResults] = useState(false);
 
 	//Get Spells from API
 	useEffect(() => {
@@ -18,43 +24,72 @@ export default () => {
 					? response.data.results
 					: [response.data.results];
 				setSpells(data);
+				setFilteredSpells(data);
 			})
 			.catch((error) => {
 				setError(error.message);
 			});
 	}, []);
 
+	useEffect(() => {
+		const elements = document.querySelectorAll('.collapsible');
+		const instance = materialize.Collapsible.init(elements);
+	}, [filteredSpells]);
+
 	//Get current spells
 	const indexOfLastSpell = currentPage * spellsPerPage;
 	const indexOfFirstSpell = indexOfLastSpell - spellsPerPage;
-	const currentSpells = spells.slice(indexOfFirstSpell, indexOfLastSpell);
+	const currentSpells = filteredSpells.slice(indexOfFirstSpell, indexOfLastSpell);
 
 	//Change page
 	const paginate = (number) => {
 		setCurrentPage(number);
 	};
 
+	const handleChange = (e) => {
+		const inputText = e.target.value.toLowerCase();
+		setSearch(inputText);
+		const spellsFromSearch = spells.filter((spell) => {
+			const spellString = JSON.stringify(spell).toLowerCase();
+			return spellString.includes(inputText);
+		});
+		setFilteredSpells(spellsFromSearch);
+	};
+
 	return (
 		<>
 			<main>
 				<h1>Spells</h1>
-				<p>Results found: {spells.length}</p>
+				<form action="" onSubmit={(event) => event.preventDefault()}>
+					<div className="input-field">
+						<i className="material-icons prefix">search</i>
+						<label htmlFor="search">Search: </label>
+						<input
+							type="text"
+							value={search}
+							id="id-search"
+							name="search"
+							onChange={handleChange}
+						/>
+					</div>
+				</form>
+				<p>Results found: {filteredSpells.length}</p>
 				<hr />
 				{currentSpells.length ? (
-					currentSpells.map((spell) => (
-						<div key={spell.name}>
-							<SpellCard spell={spell} />
-							<hr />
-						</div>
-					))
+					<ul className="collapsible">
+						{currentSpells.map((spell) => (
+							<SpellCard key={spell.name} spell={spell} />
+						))}
+					</ul>
 				) : error ? (
 					<p>{error}</p>
 				) : (
-					<h4>Loading Posts...</h4>
+					<div className="loader"></div>
 				)}
+
 				<Pagination
 					spellsPerPage={spellsPerPage}
-					totalPosts={spells.length}
+					totalSpells={filteredSpells.length}
 					paginate={paginate}
 					currentPage={currentPage}
 				/>
