c094839b2ad2715062881f3261e955ffc9f5d696
ChronoLogic <msuttie12@hotmail.com>
2022-08-26 20:23:16 +0100

    Feat: Create and style landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { IoChevronForwardSharp } from 'react-icons/io5';
+
+import css from './style.module.css';
 
 export default () => {
 	return (
 		<>
 			<main>
-				<h1>DND 5e</h1>
+				<div className="container">
+					<h1>
+						<span className="red-text">5e:</span> Home
+					</h1>
+				</div>
+				<section className="container">
+					<Link to="/spells" className={`${css.hover}`}>
+						<h4 className="black-text valign-wrapper">
+							<IoChevronForwardSharp />
+							Spells
+						</h4>
+					</Link>
+					<Link to="/conditions" className={`${css.hover}`}>
+						<h4 className="black-text valign-wrapper">
+							<IoChevronForwardSharp />
+							Conditions
+						</h4>
+					</Link>
+					<Link to="/" className={`${css.hover}`}>
+						<h4 className="grey-text valign-wrapper">
+							<IoChevronForwardSharp />
+							About
+						</h4>
+					</Link>
+				</section>
 			</main>
 		</>
 	);
