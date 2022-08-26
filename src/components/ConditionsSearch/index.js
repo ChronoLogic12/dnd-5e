d49ee458edc307a207f9be075b4d428ccc2fe8f5
ChronoLogic <msuttie12@hotmail.com>
2022-08-26 20:20:19 +0100

    Feat: Create conditions search component

---
+++ src/components/ConditionsSearch/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import css from './style.module.css';
+
+import { IoFilter } from 'react-icons/io5';
+
+export default (props) => {
+	const handleSearchInputChange = ({ target }) => {
+		props.setSearch(target.value);
+	};
+
+	return (
+		<>
+			<form className="" action="" onSubmit={(event) => event.preventDefault()}>
+				<div className="input-field valign-wrapper">
+					<i className="material-icons prefix">search</i>
+					<label htmlFor="search">Search: </label>
+					<input
+						type="text"
+						value={props.search}
+						id="search"
+						name="search"
+						onChange={handleSearchInputChange}
+					/>
+				</div>
+			</form>
+			<p>Results found: {props.conditionsCount}</p>
+		</>
+	);
+};
