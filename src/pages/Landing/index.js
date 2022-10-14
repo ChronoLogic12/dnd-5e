6db9a9866ca661c5ff13b4f9362f6803877a0de0
ChronoLogic <msuttie12@hotmail.com>
2022-10-14 17:16:51 +0100

    Style: Covert styling to new dark theme with new buttons and consistant stylised horizontal rules

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,55 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { IoChevronForwardSharp } from 'react-icons/io5';
 import { BsStars } from 'react-icons/bs';
 import { GiBackPain, GiStrong, GiIceCube, GiBookmark } from 'react-icons/gi';
 
+import { Banner } from '../../components';
 import css from './style.module.css';
 
 export default () => {
 	return (
 		<>
+			<Banner page="Home" />
 			<main>
-				<div className="container">
-					<h1>
-						<span className="red-text">5e:</span> Home
-					</h1>
-				</div>
 				<section className="container">
-					<Link to="/spells" className={`${css.hover}`}>
-						<h4 className="black-text valign-wrapper">
-							<IoChevronForwardSharp />
-							<span>Spells</span>
+					<Link to="/spells" className="hover">
+						<h4 className="custombtn valign-wrapper">
 							<BsStars className={`${css.icon} purple-text`} />
+							<span>Spells</span>
 						</h4>
 					</Link>
-					<Link to="/conditions" className={`${css.hover}`}>
-						<h4 className="black-text valign-wrapper">
-							<IoChevronForwardSharp />
-							<span>Conditions</span>
+					<Link to="/conditions" className="hover">
+						<h4 className="custombtn valign-wrapper">
 							<GiBackPain className={`${css.icon} red-text`} />
+							<span>Conditions</span>
 						</h4>
 					</Link>
-					<Link to="/feats" className={`${css.hover}`}>
-						<h4 className="black-text valign-wrapper">
-							<IoChevronForwardSharp />
-							<span>Feats</span>
+					<Link to="/feats" className="hover">
+						<h4 className="custombtn valign-wrapper">
 							<GiStrong className={`${css.icon} indigo-text`} />
+							<span>Feats</span>
 						</h4>
 					</Link>
-					<Link to="/monsters" className={`${css.hover}`}>
-						<h4 className="black-text valign-wrapper">
-							<IoChevronForwardSharp />
-							<span>Monsters</span>
+					<Link to="/monsters" className="hover">
+						<h4 className="custombtn valign-wrapper">
 							<GiIceCube className={`${css.icon} green-text`} />
+							<span>Monsters</span>
 						</h4>
 					</Link>
-					<Link to="/about" className={`${css.hover}`}>
-						<h4 className="black-text valign-wrapper">
-							<IoChevronForwardSharp />
-							About
+					<Link to="/about" className="hover">
+						<h4 className="custombtn valign-wrapper">
 							<GiBookmark className={`${css.icon} brown-text`} />
+							<span>About</span>
 						</h4>
 					</Link>
 				</section>
