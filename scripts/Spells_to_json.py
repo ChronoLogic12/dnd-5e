60ce663cf5e98a049072ed9149a3a5915f32e866
ChronoLogic <msuttie12@hotmail.com>
2024-06-04 14:21:13 +0100

    Maint: Save spells from api to data folder. Add missing spells from import

--- scripts/Spells_to_json.py
+++ scripts/Spells_to_json.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-r = requests.get(url = 'https://api.open5e.com/spells/?ordering=level_int&limit=1000')
+r = requests.get(url = 'https://api.open5e.com/spells/?ordering=level_int&limit=5000')
 data = r.json()
-data_string = json.dumps(data)
+data_string = json.dumps(data['results'])
 
-WriteFile = open('spells_from_api.json', 'w')
+WriteFile = open('data/spells_from_api.json', 'w')
 WriteFile.write(data_string)
 WriteFile.close()
